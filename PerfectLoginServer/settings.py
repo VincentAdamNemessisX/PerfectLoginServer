e09715c92558c12ec1ad8770392cce20ba031685
vincent <vincentadamnemessis@gmail.com>
2023-11-19 18:13:50 +0800

    import bootstrap and jquery but it is lack of some css style

--- PerfectLoginServer/settings.py
+++ PerfectLoginServer/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'PerfectLoginServer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../mainLogin/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
